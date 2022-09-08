d9ffffebba06c8a8045367c55582461bd75a7471
Ivan Rodriguez <sickpunkrocker@gmail.com>
2022-09-08 16:19:54 -0700

    Added end page to HTML file and updated CSS

--- assets/javascript/main-script.js
+++ assets/javascript/main-script.js
@@ -4,6 +4,5 @@ console.dir(document.body.children[0].children[2]) //Timer id="coundown-timer"
 Object.getPrototypeOf(new Document())
 
 
-
 var timerCountDown = document.getElementById("countdown-timer");
 timerCountDown.style.color = "red"
