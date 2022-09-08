91446322c04ad17bc41554298297e0fb6ec5c2d9
Ivan Rodriguez <sickpunkrocker@gmail.com>
2022-09-08 15:57:34 -0700

    Added id's and classes to HTML and CSS files, Added questions in separete javascript file and linked it to the HTML file

---
+++ assets/javascript/main-script.js
@@ -0,0 +1,9 @@
+console.log(window);
+console.log(window.document);
+console.dir(document.body.children[0].children[2]) //Timer id="coundown-timer"
+Object.getPrototypeOf(new Document())
+
+
+
+var timerCountDown = document.getElementById("countdown-timer");
+timerCountDown.style.color = "red"
---
+++ assets/javascript/questions-script.js
@@ -0,0 +1,43 @@
+// Multiple choices questions, choices and answers
+var questions = [
+    {
+        title: "HTML tags are used to describe document _________.",
+        choices: ["Definition", "Language", "Content", "None of the above"],
+        answer: "Content"
+    },
+    {
+        title: "The HTML document can contain?",
+        choices: ["Attributes", "Tags", "Plain text", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        title: "Which of the following keywords is used to define a variable in Javascript?",
+        choices: ["var", "let", "Both A & B", "None of the above"],
+        answer: "Both A & B"
+    },
+    {
+        title: "Which of the following methods is used to access HTML elements using Javascript?",
+        choices: ["getElementbyIed()", "getElementByClassName()", "Both A & B", "None of the above"],
+        answer: "Both A & B"
+    },
+    {
+        title: "The full form of CSS is?",
+        choices: ["Colored Special Sheets", "Cascading Style Sheet", "Color and Style Sheets", "None of the above"],
+        answer: "Cascading Style Sheet"
+    },
+    {
+        title: "Which HTML tag is used to declare internal CSS?",
+        choices: ["<style>", "<link>", "<script>", "None of the above"],
+        answer: "<style>"
+    },
+    {
+        title: "A very useful tool used during development and debugging is?",
+        choices: ["for loops", "Javascript", "console.log()", "None of the above"],
+        answer: "console.log()"
+    },
+    {
+        title: "Arrays can be used to store ________.",
+        choices: ["Strings", "Booleans", "Arrays", "All of the above"],
+        answer: "All of the above"
+    }
+];
