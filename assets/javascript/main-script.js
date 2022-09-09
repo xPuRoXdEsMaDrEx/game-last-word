4b9bbcc2aba16b51b782791d6bc03955010754fa
Ivan Rodriguez <sickpunkrocker@gmail.com>
2022-09-08 17:09:38 -0700

    Added variables to be used in main javascript file

--- assets/javascript/main-script.js
+++ assets/javascript/main-script.js
@@ -4,5 +4,12 @@ console.dir(document.body.children[0].children[2]) //Timer id="coundown-timer"
 Object.getPrototypeOf(new Document())
 
 
-var timerCountDown = document.getElementById("countdown-timer");
+var timerCountDown = document.getElementById('countdown-timer');
 timerCountDown.style.color = "red"
+var quizStartPage = document.getElementById('start-screen');
+var quizStartButton = document.getElementById('start-button');
+var quizQuestions = document.getElementById('questions');
+var quizQuestionTitle = document.getElementById('questions-title');
+var quizQuestionChoice = document.getElementById('questions-choice');
+var quizEndPage = document.getElementById('end-screen');
+
